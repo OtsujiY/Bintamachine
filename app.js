386fcf351da4174fc21ddc8d0e15b44d80b69032
OtsujiY <kmc2609@kamiyama.ac.jp>
2026-04-19 11:09:34 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+const userVideo = document.getElementById("camera");
+
+const cameraSetting = {
+    audio: false,
+    video: {
+        width: 200,
+        height: 200,
+        facingMode: "environment"
+    }
+}
+
+const cameraStart = () => {
+    navigator.mediaDevices.getUserMedia(cameraSetting).then((stream) => {
+        userVideo.srcObject = stream;
+    }).catch((err) => {
+        console.log(err.toString());
+    });
+}
+
+const $bintaHand = document.getElementById("binta_hand");
+
+const handLeft = ["calc(50% - 450px)", "calc(50% + 50px)"];
+let isHandUp = true;
+
+const binta = () => {
+    setTimeout(() => {
+        isHandUp = Boolean(-isHandUp + 1);
+
+        $bintaHand.style.left = handLeft[Number(isHandUp)];
+        $bintaHand.src = "images/IMG_403" + ( 4 + -isHandUp ) + ".PNG";
+    }, 70);
+};
+
+cameraStart();
+
+document.addEventListener("click", () => {
+    binta();
+});
+
+document.addEventListener("keydown", (e) => {
+    if (e.code === "Space") {
+        binta();
+    }
+})
